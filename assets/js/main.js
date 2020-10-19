5c9196532de0b026f395bfd9facee08898556b80
Darkdreamnit <49699265+Darkdreamnit@users.noreply.github.com>
2020-10-19 11:16:50 -0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -63,3 +63,12 @@
 				});
 
 })(jQuery);
+
+$(document).ready(function(){
+	$('.acc h3').click(function(){
+		$(this).next('.content').slideToggle();
+		$(this).parent().toggleClass('active');
+		$(this).parent().siblings().children('.content').slideUp();
+		$(this).parent().siblings().removeClass('active');
+	});
+});
